61e60b7d6bedb4c55ae980506c7be7aa4334fd32
Osvaldo Villalba Mendoza <osvaldovillalba-02@hotmail.com>
2026-06-27 16:48:18 -0600

    Agregar la variable SUPABASE_JWT_SECRET a la configuración de Settings

--- app/core/config.py
+++ app/core/config.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     SUPABASE_URL: str
     SUPABASE_KEY: str
+    SUPABASE_JWT_SECRET: str
 
     class Config:
         env_file = ".env"
